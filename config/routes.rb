5afb362c6c1e0b47df03025272c68591cd348de9
7stud <7stud@excite.com>
2012-08-29 19:56:26 -0700

    Stuck on StaticPagesController problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp25::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/non_exist"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
